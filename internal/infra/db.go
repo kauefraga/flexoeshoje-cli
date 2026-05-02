3c8989b0a3ae9e95f8fe2a64355b253aaa57b481
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2026-05-02 17:18:54 -0300

    refactor: move table creation to db.go and remove unused function

--- internal/infra/db.go
+++ internal/infra/db.go
@@ -14,3 +14,16 @@ func ConnectToDB() *sql.DB {
 
 	return db
 }
+
+func CreatePushupsTable(db *sql.DB) {
+	query := `CREATE TABLE IF NOT EXISTS pushups (
+		id INTEGER PRIMARY KEY,
+		repetitions INTEGER NOT NULL,
+		last_modified TEXT DEFAULT CURRENT_TIMESTAMP,
+		created_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP
+	)`
+
+	if _, err := db.Exec(query); err != nil {
+		log.Fatal("Ocorreu um erro ao criar a tabela de flexões.")
+	}
+}
--- internal/infra/queries.go
+++ internal/infra/queries.go
@@ -6,19 +6,6 @@ import (
 	"time"
 )
 
-func CreatePushupsTable(db *sql.DB) {
-	query := `CREATE TABLE IF NOT EXISTS pushups (
-		id INTEGER PRIMARY KEY,
-		repetitions INTEGER NOT NULL,
-		last_modified TEXT DEFAULT CURRENT_TIMESTAMP,
-		created_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP
-	)`
-
-	if _, err := db.Exec(query); err != nil {
-		log.Fatal("Ocorreu um erro ao criar a tabela de flexões.")
-	}
-}
-
 func FindTodayPushups(db *sql.DB) int {
 	var total int
 
@@ -31,10 +18,6 @@ func FindTodayPushups(db *sql.DB) int {
 	}
 
 	return total
-}
-
-func FindManyPushups(db *sql.DB) {
-
 }
 
 func CreateOnePushup(db *sql.DB, repetitions int) error {
